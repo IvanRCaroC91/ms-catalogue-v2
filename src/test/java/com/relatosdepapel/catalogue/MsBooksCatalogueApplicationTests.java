59ab12089cd144618e575a9a93938ad78d2b69e9
IVAN RENE CARO CATAÑO <ivan-caro-123@hotmail.com>
2025-06-09 22:38:05 -0500

    Commit inicial: configuración del proyecto Spring Boot

---
+++ src/test/java/com/relatosdepapel/catalogue/MsBooksCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatosdepapel.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBooksCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
